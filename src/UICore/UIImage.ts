bac12b434dfdd995910b3b5fead3022defd145f8
nExmond <rnpfr254@gmail.com>
2021-03-15 01:55:15 +0900

    Add Image constants

--- src/UICore/UIImage.ts
+++ src/UICore/UIImage.ts
@@ -62,6 +62,12 @@ class UIImage {
         return this;
     }
 
+    /**
+     * Offsets uiimage
+     * * Currently, it is only valid for images applied to tabs.
+     * @param val 
+     * @returns offset 
+     */
     offset(val: UIPoint): this {
         this._offset = val;
         return this;
@@ -85,8 +91,81 @@ class UIImage {
 //UIImage constants
 const UIImageNone = UIImage.$(-1);
 
+//Shop Item
+const UIImageShopItemBalloon = UIImage.$(5061);
+const UIImageShopItemToy = UIImage.$(5062);
+const UIImageShopItemMap = UIImage.$(5063);
+const UIImageShopItemPhoto = UIImage.$(5064);
+const UIImageShopItemUmbrella = UIImage.$(5065);
+const UIImageShopItemDrink = UIImage.$(5066);
+const UIImageShopItemBurger = UIImage.$(5067);
+const UIImageShopItemChips = UIImage.$(5068);
+const UIImageShopItemIceCream = UIImage.$(5069);
+const UIImageShopItemCandyfloss = UIImage.$(5070);
+const UIImageShopItemEmptyCan = UIImage.$(5071);
+const UIImageShopItemRubbish = UIImage.$(5072);
+const UIImageShopItemEmptyBurgerBox = UIImage.$(5073);
+const UIImageShopItemPizza = UIImage.$(5074);
+const UIImageShopItemVoucher = UIImage.$(5075);
+const UIImageShopItemPopcorn = UIImage.$(5076);
+const UIImageShopItemHotDog = UIImage.$(5077);
+const UIImageShopItemTentacle = UIImage.$(5078);
+const UIImageShopItemHat = UIImage.$(5079);
+const UIImageShopItemToffeeApple = UIImage.$(5080);
+const UIImageShopItemTshirt = UIImage.$(5081);
+const UIImageShopItemDoughnut = UIImage.$(5082);
+const UIImageShopItemCoffee = UIImage.$(5083);
+const UIImageShopItemEmptyCup = UIImage.$(5084);
+const UIImageShopItemChicken = UIImage.$(5085);
+const UIImageShopItemLemonade = UIImage.$(5086);
+const UIImageShopItemEmptyBox = UIImage.$(5087);
+const UIImageShopItemEmptyBottle = UIImage.$(5088);
+const UIImageShopItemPhoto2 = UIImage.$(5089);
+const UIImageShopItemPhoto3 = UIImage.$(5090);
+const UIImageShopItemPhoto4 = UIImage.$(5091);
+const UIImageShopItemPretzel = UIImage.$(5092);
+const UIImageShopItemChocolate = UIImage.$(5093);
+const UIImageShopItemIcedTea = UIImage.$(5094);
+const UIImageShopItemFunnelCake = UIImage.$(5095);
+const UIImageShopItemSunglasses = UIImage.$(5096);
+const UIImageShopItemBeefNoodles = UIImage.$(5097);
+const UIImageShopItemFriedRiceNoodles = UIImage.$(5098);
+const UIImageShopItemWontonSoup = UIImage.$(5099);
+const UIImageShopItemMeatballSoup = UIImage.$(5100);
+const UIImageShopItemFruitJuice = UIImage.$(5101);
+const UIImageShopItemSoybeanMilk = UIImage.$(5102);
+const UIImageShopItemSujeonggwa = UIImage.$(5103);
+const UIImageShopItemSubSandwich = UIImage.$(5104);
+const UIImageShopItemCookie = UIImage.$(5105);
+const UIImageShopItemEmptyBowlRed = UIImage.$(5106);
+const UIImageShopItemEmptyDrinkCarton = UIImage.$(5107);
+const UIImageShopItemEmptyJuiceCup = UIImage.$(5108);
+const UIImageShopItemRoastSausage = UIImage.$(5109);
+const UIImageShopItemEmptyBowlBlue = UIImage.$(5110);
 
+//Staff
+const UIImageStaffOrdersSweeping = UIImage.$(5111);
+const UIImageStaffOrdersWaterFlowers = UIImage.$(5112);
+const UIImageStaffOrdersEmptyBins = UIImage.$(5113);
+const UIImageStaffOrdersMowing = UIImage.$(5114);
+const UIImageStaffOrdersInspectRides = UIImage.$(5115);
+const UIImageStaffOrdersFixRides = UIImage.$(5116);
+const UIImageStaffPatrolPath = UIImage.$(5117);
+const UIImageStaffCostumePanda = UIImage.$(5118);
+const UIImageStaffCostumeTiger = UIImage.$(5119);
+const UIImageStaffCostumeElephant = UIImage.$(5120);
+const UIImageStaffCostumeRoman = UIImage.$(5121);
+const UIImageStaffCostumeGorilla = UIImage.$(5122);
+const UIImageStaffCostumeSnowman = UIImage.$(5123);
+const UIImageStaffCostumeKnight = UIImage.$(5124);
+const UIImageStaffCostumeAstronaut = UIImage.$(5125);
+const UIImageStaffCostumeBandit = UIImage.$(5126);
+const UIImageStaffCostumeSheriff = UIImage.$(5127);
+const UIImageStaffCostumePirate = UIImage.$(5128);
 
+const UIImageInformationSmall = UIImage.$(5129);
+const UIImageRatingIncrease = UIImage.$(5130);
+const UIImageRatingDecrease = UIImage.$(5131);
 
 //Ride
 const UIImageRideConstructionStraight = UIImage.$(5137);
@@ -202,8 +281,24 @@ const UIImageTabRidesWater = UIImage.$A(5551, 6, 4);
 const UIImageTabRidesThrill = UIImage.$F([5562, 5563, 5562, 5561, 5560, 5559, 5558, 5557, 5557, 5557, 5557, 5557, 5557, 5557, 5557, 5557, 5558, 5559, 5560, 5561, ], 4)
 const UIImageTabGuests = UIImage.$A(5568, 8, 4);
 
-const UIImageTabLand = UIImage.$(29362);
-const UIImageTabNews = UIImage.$(29414);
+//Award
+const UIImageAwardMostUntidy = UIImage.$(5469);
+const UIImageAwardMostTidy = UIImage.$(5470);
+const UIImageAwardBestRollerCoasters = UIImage.$(5471);
+const UIImageAwardBestValue = UIImage.$(5472);
+const UIImageAwardMostBeautiful = UIImage.$(5473);
+const UIImageAwardWorstValue = UIImage.$(5474);
+const UIImageAwardSafest = UIImage.$(5475);
+const UIImageAwardBestStaff = UIImage.$(5476);
+const UIImageAwardBestFood = UIImage.$(5477);
+const UIImageAwardWorstFood = UIImage.$(5478);
+const UIImageAwardBestRestrooms = UIImage.$(5479);
+const UIImageAwardMostDisappointing = UIImage.$(5480);
+const UIImageAwardBestWaterRides = UIImage.$(5481);
+const UIImageAwardBestCustomDesignedRides = UIImage.$(5482);
+const UIImageAwardMostDazzlingRideColors = UIImage.$(5483);
+const UIImageAwardMostConfusingLayout = UIImage.$(5484);
+const UIImageAwardBestGentleRides = UIImage.$(5485);
 
 //Peep
 const UIImagePeepLargeFaceVeryVeryUnhappy = UIImage.$(5284);
@@ -220,6 +315,20 @@ const UIImagePeepLargeFaceVerySick = UIImage.$A(5294, 4, 8);
 const UIImagePeepLargeFaceVeryVerySick = UIImage.$A(5298, 16, 4);
 const UIImagePeepLargeFaceAngry = UIImage.$A(5314, 4, 8);
 
+const UIImagePeepSmallFaceVeryVeryUnhappy = UIImage.$(5486);
+const UIImagePeepSmallFaceVeryUnhappy = UIImage.$(5487);
+const UIImagePeepSmallFaceUnhappy = UIImage.$(5488);
+const UIImagePeepSmallFaceNormal = UIImage.$(5489);
+const UIImagePeepSmallFaceHappy = UIImage.$(5490);
+const UIImagePeepSmallFaceVeryHappy = UIImage.$(5491);
+const UIImagePeepSmallFaceVeryVeryHappy = UIImage.$(5492);
+const UIImagePeepSmallFaceTired = UIImage.$(5493);
+const UIImagePeepSmallFaceVeryTired = UIImage.$(5494);
+const UIImagePeepSmallFaceSick = UIImage.$(5495);
+const UIImagePeepSmallFaceVerySick = UIImage.$(5496);
+const UIImagePeepSmallFaceVeryVerySick = UIImage.$(5497);
+const UIImagePeepSmallFaceAngry = UIImage.$(5498);
+
 //Texture
 const UIImageFloorTextureGrass = UIImage.$(5579);
 const UIImageFloorTextureSand = UIImage.$(5580);
@@ -231,7 +340,7 @@ const UIImageFloorTextureGrassClumps = UIImage.$(5585);
 const UIImageFloorTextureIce = UIImage.$(5586);
 const UIImageFloorTextureGridRed = UIImage.$(5587);
 const UIImageFloorTextureGridYellow = UIImage.$(5588);
-const UIImageFloorTextureGridBlue = UIImage.$(5589);
+const UIImageFloorTextureGridPurple = UIImage.$(5589);
 const UIImageFloorTextureGridGreen = UIImage.$(5590);
 const UIImageFloorTextureSandDark = UIImage.$(5591);
 const UIImageFloorTextureSandLight = UIImage.$(5592);
@@ -248,6 +357,36 @@ const UIImageConstructionDirectionSW = UIImage.$(5637);
 const UIImageConstructionDirectionNW = UIImage.$(5638);
 const UIImageConstructionFootpathLand = UIImage.$(5639);
 const UIImageConstructionFootpathBridge = UIImage.$(5640);
+
+//Maze Wall
+const UIImageRideMazeWallHedgeThickFull = UIImage.$(21938);
+const UIImageRideMazeWallHedgeThickSWNE = UIImage.$(21939);
+const UIImageRideMazeWallHedgeThickNWSE = UIImage.$(21940);
+const UIImageRideMazeWallHedgeMediumSWNE = UIImage.$(21941);
+const UIImageRideMazeWallHedgeMediumNWSE = UIImage.$(21942);
+const UIImageRideMazeWallHedgeThinSWNE = UIImage.$(21943);
+const UIImageRideMazeWallHedgeThinNWSE = UIImage.$(21944);
+const UIImageRideMazeWallBrickThickFull = UIImage.$(21951);
+const UIImageRideMazeWallBrickThickSWNE = UIImage.$(21952);
+const UIImageRideMazeWallBrickThickNWSE = UIImage.$(21953);
+const UIImageRideMazeWallBrickMediumSWNE = UIImage.$(21954);
+const UIImageRideMazeWallBrickMediumNWSE = UIImage.$(21955);
+const UIImageRideMazeWallBrickThinSWNE = UIImage.$(21956);
+const UIImageRideMazeWallBrickThinNWSE = UIImage.$(21957);
+const UIImageRideMazeWallIceThickFull = UIImage.$(21964);
+const UIImageRideMazeWallIceThickSWNE = UIImage.$(21965);
+const UIImageRideMazeWallIceThickNWSE = UIImage.$(21966);
+const UIImageRideMazeWallIceMediumSWNE = UIImage.$(21967);
+const UIImageRideMazeWallIceMediumNWSE = UIImage.$(21968);
+const UIImageRideMazeWallIceThinSWNE = UIImage.$(21969);
+const UIImageRideMazeWallIceThinNWSE = UIImage.$(21970);
+const UIImageRideMazeWallWoodenThickFull = UIImage.$(21977);
+const UIImageRideMazeWallWoodenThickSWNE = UIImage.$(21978);
+const UIImageRideMazeWallWoodenThickNWSE = UIImage.$(21979);
+const UIImageRideMazeWallWoodenMediumSWNE = UIImage.$(21980);
+const UIImageRideMazeWallWoodenMediumNWSE = UIImage.$(21981);
+const UIImageRideMazeWallWoodenThinSWNE = UIImage.$(21982);
+const UIImageRideMazeWallWoodenThinNWSE = UIImage.$(21983);
 
 //??
 const UIImageFirecracker = UIImage.$A(22927, 28, 2);
@@ -345,8 +484,66 @@ const UIImageFlatTextureIce4 = UIImage.$(29010);
 const UIImageFlatTextureIce5 = UIImage.$(29011);
 const UIImageFlatTextureIce6 = UIImage.$(29012);
 
-//29357부터
+const UIImageWallMedievalGray = UIImage.$(112511);
+const UIImageWallTin = UIImage.$(124211);
+const UIImageWallSandBrick = UIImage.$(152747);
+const UIImageWallRustyTin = UIImage.$(163772);
+const UIImageWallSand = UIImage.$(163784);
+const UIImageWallLightSand = UIImage.$(168735);
+const UIImageWallBordeauxRed = UIImage.$(171630);
+const UIImageWallViolet = UIImage.$(171641);
 
+const UIImageG2Logo = UIImage.$(29357);
+const UIImageG2Title = UIImage.$(29358);
 
+const UIImageG2Fastforward = UIImage.$(29359);
+const UIImageG2SpeedArrow = UIImage.$(29360);
+const UIImageG2HyperArrow = UIImage.$(29361);
 
-// const UIImage = UIImage.$();
+const UIImageG2TabLand = UIImage.$(29362);
+
+const UIImageG2Placeholder = UIImage.$(29363);
+
+const UIImageG2ZoomIn = UIImage.$(29364);
+const UIImageG2ZoomOut = UIImage.$(29366);
+
+const UIImageG2TabTree = UIImage.$(29368);
+const UIImageG2TabPencil = UIImage.$(29369);
+const UIImageG2LargeScenery = UIImage.$(29370);
+const UIImageG2Trees = UIImage.$(29371);
+const UIImageG2Footpath = UIImage.$(29372);
+
+const UIImageG1CloseDisable = UIImage.$(29373);
+const UIImageG1CloseEnable = UIImage.$(29375);
+const UIImageG1TestDisable = UIImage.$(29377);
+const UIImageG1TestEnable = UIImage.$(29379);
+const UIImageG1OpenDisable = UIImage.$(29381);
+const UIImageG1OpenEnable = UIImage.$(29383);
+const UIImageG2Simulate = UIImage.$(29481);
+const UIImageG1SimulateDisable = UIImage.$(29482);
+const UIImageG1SimulateEnable = UIImage.$(29483);
+
+const UIImageG2Restart = UIImage.$(29385);
+const UIImageG2Stop = UIImage.$(29386);
+const UIImageG2Play = UIImage.$(29387);
+const UIImageG2Skip = UIImage.$(29388);
+
+const UIImageG2Sandbox = UIImage.$(29389);
+const UIImageG2TabNews = UIImage.$(29414);
+
+const UIImageG2Locked = UIImage.$(29415);
+const UIImageG2MenuMultiplayer = UIImage.$(29416);
+
+const UIImageG2Sort = UIImage.$(29433);
+const UIImageG2Copy = UIImage.$(29434);
+const UIImageG2Paste = UIImage.$(29435);
+
+const UIImageG2Search = UIImage.$(29461);
+const UIImageG2Pipette = UIImage.$(29467);
+const UIImageG2Chat = UIImage.$(29468);
+
+const UIImageG2SceneryScatterLow = UIImage.$(29489);
+const UIImageG2SceneryScatterMedium = UIImage.$(29490);
+const UIImageG2SceneryScatterHigh = UIImage.$(29491);
+
+const UIImageG2View = UIImage.$(29494);
