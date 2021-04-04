da156ee4e7d8ac874a724df59e27f40074aaac30
nExmond <rnpfr254@gmail.com>
2021-04-04 19:59:28 +0900

    -

--- demo/StackUI-Demo.ts
+++ demo/StackUI-Demo.ts
@@ -2,7 +2,62 @@
 
 var openWindow = function () {
 
-    UIWindow.$T("")
+    //proxy
+    const window = UIWDP.$();
+    const tab1 = UITP.$();
+
+    const buttonBasicTitle = UIWP.$<UIButton>();
+    const buttonBasicImage = UIWP.$<UIButton>();
+    const buttonToggleTitle = UIWP.$<UIToggleButton>();
+    const buttonToggleImage = UIWP.$<UIToggleButton>();
+    const buttonPagingImage = UIWP.$<UIPageImageButton>();
+
+    const temp = UIWP.$<UIButton>();
+
+    //construct
+    UIWindow.$T("StackUI Demo",
+        UITab.$(
+            UIButton.$("dddddd"),
+            UISpacer.$(),
+            UIStack.$H(
+                UIStack.$V(
+                    UIButton.$("button").bind(buttonBasicTitle),
+                    UIButton.$I(UIImageGuests).bind(buttonBasicImage),
+                ).title("Basic"),
+                UIStack.$V(
+                    UIToggleButton.$("button").bind(buttonToggleTitle),
+                    UIToggleButton.$I(UIImageGuests).bind(buttonToggleImage),
+                ).title("Toggle"),
+                UISpacer.$(),
+                UIStack.$V(
+                    UIPageImageButton.$IP(...[
+                        UIImageAwardBestStaff,
+                        UIImageAwardBestValue, 
+                        UIImageAwardMostBeautiful
+                    ]).bind(buttonPagingImage)
+                ).title("Paging")
+            ).title("Button")
+        ).bind(tab1)
+        .isExpandable(true)
+        .image(UIImageConstruction.offset({ x: 4, y: 2 })),
+        UITab.$(
+            UIViewport.$(),
+            UISpacer.$(),
+            UIButton.$("탭 인덱스").bind(temp),
+            UICheckbox.$("체크박스"),
+            UIDropdown.$(["1", "2"]),
+            UIColorPicker.$(),
+            UISpinner.$(),
+            UITextBox.$(),
+            UIListView.$([UIListViewColumn.$("속성")]).addItem(UIListViewItem.$(["값"]))
+        ).image(UIImageTabAwards).isExpandable(true)
+    ).bind(window)
+    .theme({ primary: UIColor.Gray, secondary: UIColor.DarkYellow })
+    .padding({ top: 2, left: 2, bottom: 2, right: 2 })
+    .spacing(2)
+    .show()
+
+    //action
 };
 
 var main = function () {
